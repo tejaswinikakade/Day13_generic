d2a3889c79b9f194cf1f7b163aa7bb76e7535989
tejaswinikakade <tejukakade1821@gmail.com>
2021-11-02 01:11:27 +0530

    3 Integers find

---
+++ Generic_day13/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Generic_day13
+{
+    class Program
+    {
+        public static object TC1Integer { get; private set; }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Find Max Value");
+
+            int output = UC1.Maxint(11, 22, 33);
+            Console.WriteLine(output);
+        }
+    }
+}
---
+++ Generic_day13/UC1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Generic_day13
+{
+    class UC1
+    {
+        public static int Maxint(int First, int second, int third)
+        {
+            if (First.CompareTo(second) > 0 && First.CompareTo(third) > 0)
+            {
+                return First;
+
+            }
+
+            if (second.CompareTo(First) > 0 && second.CompareTo(third) > 0)
+            {
+                return second;
+            }
+
+            if (third.CompareTo(second) > 0 && third.CompareTo(second) > 0)
+            {
+                return third;
+            }
+
+            throw new Exception("First Number , Second Number , Third Number are Same");
+        }
+
+    }
+}
